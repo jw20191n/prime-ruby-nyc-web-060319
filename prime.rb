529e0df9e848d781bfcdcca68a7d6e067ca77015
Jiayi Wang <clairejw1015@gmail.com>
2019-05-22 20:57:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,10 @@ def prime?(int)
   
   if int <= 1
     return false
+  elsif int == 2
+    return true
   elsif
-  
-    for i in 2..int do
+    for i in 3..int do
       
       if int % i == 0
         return false
